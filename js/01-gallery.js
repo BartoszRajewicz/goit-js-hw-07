100d50dd84b9f1858b393f23cd2b9a73918f6772
BartoszRajewicz <szejn3@gmail.com>
2023-11-10 00:24:38 +0100

    added js script for first part of showing gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,19 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryContainer = document.querySelector("ul.gallery");
+galleryContainer.addEventListener("click", (event) => {
+  event.preventDefault();
+});
+const images = galleryItems.map((item) => {
+  return `<div class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                 <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                />
+             </a>
+        </div>`;
+});
+galleryContainer.insertAdjacentHTML("beforeend", images.join(""));
