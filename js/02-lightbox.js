a3ff756a4febc8454d5f136c042cefde0541b078
BartoszRajewicz <szejn3@gmail.com>
2023-11-12 15:49:04 +0100

    added the js code

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector("ul.gallery");
+
+const images = galleryItems.map((item) => {
+  return `
+    <li class ="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+                 <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                />
+             </a>
+    </li>`;
+});
+
+galleryContainer.insertAdjacentHTML("beforeend", images.join(""));
+
+galleryContainer.addEventListener("click", (event) => {
+  event.preventDefault();
+  const image = event.target.closest(".gallery__image");
+});
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
