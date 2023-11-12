34aa6b967f159f3dc5d4b4d3eb0aa29a8f06f1e9
BartoszRajewicz <szejn3@gmail.com>
2023-11-12 15:03:13 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,6 +2,12 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryContainer = document.querySelector("ul.gallery");
+
+const openLightbox = (source, alt) => {
+  const lightbox = basicLightbox.create(`<img src="${source}" alt="${alt}">`);
+  lightbox.show();
+};
+
 const images = galleryItems.map((item) => {
   return `<div class="gallery__item">
             <a class="gallery__link" href="${item.original}">
@@ -17,6 +23,14 @@ const images = galleryItems.map((item) => {
 
 galleryContainer.insertAdjacentHTML("beforeend", images.join(""));
 
-galleryItems.addEventListener("click", (event) => {
+galleryContainer.addEventListener("click", (event) => {
   event.preventDefault();
+  const image = event.target.closest(".gallery__image");
+
+  if (image) {
+    const source = image.dataset.source;
+    const alt = image.alt;
+    openLightbox(source, alt);
+  }
 });
+
